7aac0e6516cf6499eeb4ad3a06f874491d6b0576
Kogrisen <marcus.moonlit@outlook.com>
2022-03-16 21:56:26 +0100

    Initial commit

---
+++ mediaInheritance/Media.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mediaInheritance
+{
+    internal abstract class Media
+    {
+        protected string title;
+        protected string release_year;
+        //The creator is in case of a book the author, and for films it is the director. For games it's the studio
+        protected string creator;
+    }
+
+    internal class Film : Media
+    {
+        private string studio;
+        protected int length;
+
+        public override string ToString()
+        {
+            return String.Format("Name; {{0}}\n" +
+                "Release year: {{1}}\n" +
+                "Director: {{2}}\n" +
+                "Runtime: {{3}}\n" +
+                "Studio: {{4}}", title, release_year, creator, length, studio);
+        }
+    }
+
+    internal class Book : Media
+    {
+        public Book(string title, string release_year, string author, int pages, string publisher)
+        {
+            this.title = title;
+            this.release_year = release_year;
+            this.creator = author;
+            this.length = pages;
+            this.publisher = publisher;
+        }
+        private bool author_known;
+        private string publisher;
+        protected int length;
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0}\n" +
+                "Release year: {1}\n" +
+                "Author: {2}\n" +
+                "Number of pages: {3}\n" +
+                "Publisher: {4}\n", title, release_year, creator, length, publisher);
+        }
+    }
+
+    internal class Game : Media
+    {
+        private bool multiplayer;
+        private string publisher;
+        string genre;
+        public override string ToString()
+        {
+            return String.Format("Name; {{0}}\n" +
+                "Genre: {{1}}" +
+                "Release year: {{2}}\n" +
+                "Multiplayer: {{3}}\n" +
+                "Studio: {{4}}\n" +
+                "Publisher: {{5}}", title, genre, release_year, multiplayer, creator, publisher);
+        }
+    }
+}
---
+++ mediaInheritance/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mediaInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Media> archive_item = new List<Media>();
+            bool quit = false;
+            char user_choice;
+
+            while (quit == false)
+            {
+                Console.WriteLine("What would you like to enter:\n1. a book\n2. a Film\n3. a Game\n4. View list\n5. exit");
+                char.TryParse(Console.ReadLine(), out user_choice);
+
+                switch (user_choice)
+                {
+                    case '1':
+                        Console.Write("Title: ");
+                        string temp_name = Console.ReadLine().ToString();
+                        Console.Write("Author: ");
+                        string temp_author = Console.ReadLine().ToString();
+                        Console.Write("Number of pages: ");
+                        int temp_length = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Year first published: ");
+                        string temp_year = Console.ReadLine();
+                        Console.WriteLine("Publisher: ");
+                        string temp_publisher = Console.ReadLine().ToString();
+                        archive_item.Add(new Book(temp_name, temp_year, temp_author, temp_length, temp_publisher));
+                        break;
+                    case '2':
+                        //add a film
+                        break;
+                    case '3':
+                        //add a game
+                        break;
+                    case '4':
+                        //view list
+                        for (int current_item = 0; current_item < archive_item.Count; current_item++)
+                        {
+                            Console.WriteLine(archive_item[current_item].ToString());
+                        }
+                        break;
+                    case '5':
+                        //exit
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
